24d77d3960956e5df016431ad94165f9ed0962a8
Niek Vlam <vlam4695@gmail.com>
2020-05-16 08:44:20 +0200

    Added Postgresql with a query that creates a table if it doesn't exist yet

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
 
+const { Pool, Client } = require('pg');
+// pools will use environment variables
+// for connection information
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'gluchatapp',
+  password: 'wbbRf3&R^P$q6xV*4W42iL@t',
+  port: 5432,
+});
+
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
@@ -38,4 +49,9 @@ tech.on('connection', (socket) => {
   socket.on('disconnect', () => {
     tech.emit('message', 'user disconnected');
   });
+});
+
+// Postgres
+pool.query('CREATE TABLE IF NOT EXISTS users (id int PRIMARY KEY, username varchar(45) NOT NULL, password varchar(450) NOT NULL)', (err, res) => {
+  pool.end();
 });
