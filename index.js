feb0542c9596f94cb6f4011bec9beaa483180592
Niek Vlam <vlam4695@gmail.com>
2020-05-20 19:13:36 +0200

    Fixed port not registering in Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const { Pool, Client } = require('pg');
 // pools will use environment variables
